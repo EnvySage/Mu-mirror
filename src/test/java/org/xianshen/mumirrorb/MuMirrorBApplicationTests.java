770b7232e1c4f8a0977c86d3ec538d4fe7f1f374
羡笙z <14679730+xiansheng-z@user.noreply.gitee.com>
2026-08-04 21:23:44 +0800

    Initial commit: Spring Boot project setup with Maven

---
+++ src/test/java/org/xianshen/mumirrorb/MuMirrorBApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xianshen.mumirrorb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuMirrorBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
